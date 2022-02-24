c086d013cadf315ffa64e32af6f34d833bb741e6
Choe Hwal <41378264+duodecanol@users.noreply.github.com>
2022-02-24 20:01:07 +0900

    Create Utils.vb

---
+++ Utils.vb
@@ -0,0 +1,21 @@
+Option Explicit
+
+Public Sub ResetFRcond()
+' Reset find & replace conditions
+    Selection.Find.ClearFormatting
+    Selection.Find.Replacement.ClearFormatting
+    With Selection.Find
+        .Text = ""
+        .Replacement.Text = ""
+        .Forward = True
+        .Wrap = wdFindContinue
+        .Format = False
+        .MatchCase = False
+        .MatchWholeWord = False
+        .MatchByte = False
+        .MatchAllWordForms = False
+        .MatchSoundsLike = False
+        .MatchWildcards = False
+        .MatchFuzzy = False
+    End With
+End Sub
